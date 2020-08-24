56766e51bb7fc8159e8c87b66e3e599eaf7111ce
u101004132 <dzag17188@gmail.com>
2020-08-24 01:41:37 -0400

    auth api working to log in and create and find users, logout unneeded due to redux in front end and jwt

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,10 @@
 // imports
 const router = require('express').Router();
 const ctrl = require('../controllers');
-
+const authenticate = require('../middleware/authenticate')
 // create routes
-router.post('/', ctrl.auth.index);
+router.post('/', ctrl.auth.index); // create user
+router.post('/user', authenticate, ctrl.verify.index); // find user token by log in info
 /* router.post('/', ctrl.products.create);
 router.delete('/delete/:id', ctrl.products.delItem)
 router.get('/view/:id', ctrl.products.showItem)
--- routes/verify.js
+++ routes/verify.js
@@ -1,9 +1,9 @@
 // imports
 const router = require('express').Router();
 const ctrl = require('../controllers');
-
+const authenticate = require('../middleware/authenticate')
 // create routes
-router.post('/', ctrl.verify.index)
+router.post('/', ctrl.verify.index) // log in
 /* router.post('/', ctrl.products.create);
 router.delete('/delete/:id', ctrl.products.delItem)
 router.get('/view/:id', ctrl.products.showItem)
